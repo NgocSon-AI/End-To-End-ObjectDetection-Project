680827231ad42b24da437cfd2a1f721131f8a400
NgocSon-AI <286sunsun@gmail.com>
2024-11-22 22:12:38 +0700

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # End-To-End-ObjectDetection-Project
+
+
+## How to run:
+```bash
+conda create -n signLanguage python=3.7 -y
+```
+
+```bash
+conda activate signLanguage
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version='0.0.0',
+    author='SunAI',
+    author_email='286sunsun@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
+
