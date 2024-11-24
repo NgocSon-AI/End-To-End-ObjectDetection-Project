0486a43ca3068d44c0b16425be88cd96a2ade5fa
NgocSon-AI <286sunsun@gmail.com>
2024-11-24 15:44:38 +0700

    exception code updated

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# from signLanguage.logger import logging
-# from signLanguage.exception import SignException
-# import sys
+from signLanguage.logger import logging
+from signLanguage.exception import SignException
+import sys
 
 
-# #logging.info("Welcome to the project")
-# try:
-#     a = 7 / '9'
-# except Exception as e:
-#     raise SignException(e, sys) from e
+#logging.info("Welcome to the project")
+try:
+    a = 7 / '9'
+except Exception as e:
+    raise SignException(e, sys) from e
